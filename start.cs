786c1bf14c5e2e73ae54b34c2ed0e40b962d7b90
C E Z A#6466 <93098721+cezacs1@users.noreply.github.com>
2023-11-27 03:36:29 +0300

    Create start.cs

---
+++ start.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace start
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filename = "ExsenseV2.exe";
+
+            if (!File.Exists(filename))
+            {
+                MessageBox.Show(filename + " dosyası bulunamadı.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Environment.Exit(0);
+            }
+
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = filename/*Process.GetCurrentProcess().ProcessName*/,
+                    Verb = "runas" // Bu, işlemi yönetici izinleriyle başlatmak için gereklidir
+                };
+
+                Process.Start(psi);
+            }
+            catch
+            {
+                new Thread(() =>
+                {
+                    Thread.Sleep(2750);
+                    Environment.Exit(0);
+                }).Start();
+
+                MessageBox.Show("Programı yönetici olarak başlatın.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Environment.Exit(0);
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
